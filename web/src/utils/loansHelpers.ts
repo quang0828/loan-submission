5d59810bbb6b757bf8e7ad6c465c6fbb9ebaa41e
Quang Nguyen <quang.nguyen@wizeline.com>
2020-08-14 02:32:16 +0700

    feat: format number

--- web/src/utils/loansHelpers.ts
+++ web/src/utils/loansHelpers.ts
@@ -3,7 +3,7 @@ import { interestRates, productTypeParseName } from './constants';
 
 export function monthlyAmortizationCalculator(loanAmount: number, interestRate: number, terms: number) {
   if (!loanAmount || !interestRate || !terms) {
-    return ''
+    return 0
   }
   const num = ((loanAmount *  interestRate / 100) * terms + loanAmount) / terms
   return Math.round(num * 100) / 100
@@ -14,19 +14,25 @@ function formatDateSumittedDate(time: string) {
   return `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`;
 }
 
+function numberWithCommas(number: number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
 export function processLoans(list: Array<LoanType>) {
   if (list && list.length) {
     return list.map(({ productType, loanAmount, terms, _id, createdAt }) => ({
       productType: productType ? productTypeParseName[productType] : '',
-      loanAmount,
+      loanAmount: numberWithCommas(loanAmount),
       terms: `${terms} months`,
       _id,
       submittedDate: createdAt ? formatDateSumittedDate(createdAt) : '',
       interestRate: productType ? `${interestRates[productType]}%` : '',
-      monthlyAmortization: monthlyAmortizationCalculator(
-        loanAmount,
-        productType ? interestRates[productType] : 0,
-        terms || 0
+      monthlyAmortization: numberWithCommas(
+        monthlyAmortizationCalculator(
+          loanAmount,
+          productType ? interestRates[productType] : 0,
+          terms || 0
+        )
       ),
       principalApproved: 'N/A'
     }));
